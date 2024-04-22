882a20e1e1648f916fcbc278753b51116d197b10
anotherly <anotherly91@gmail.com>
2024-04-22 21:25:49 +0900

    일반사용자 - 수동청구서 등록

--- Cloud24365/src/main/java/kr/co/hivesys/bill/service/BillService.java
+++ Cloud24365/src/main/java/kr/co/hivesys/bill/service/BillService.java
@@ -3,6 +3,7 @@ package kr.co.hivesys.bill.service;
 import java.util.List;
 
 import kr.co.hivesys.bill.vo.BillVo;
+import kr.co.hivesys.comm.file.vo.FileVo;
 
 
 public interface BillService {
@@ -19,4 +20,12 @@ public interface BillService {
 	int delete(List<String> inputList);
 
 	int firstNumber(BillVo inputVo);
+	
+	//#####################수동청구서#####################//
+	
+	List<BillVo> manualList(BillVo inputVo);
+	String createMnId(BillVo inputVo);
+	int manualInsert(BillVo inputVo);
+	
+	FileVo firstFile();
 }
